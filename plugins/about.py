8c386d9ec928b1782787eb19e8856b98fe5705e1
Distenyteam <105855119+Distenyteam@users.noreply.github.com>
2022-07-08 03:21:02 +0100

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -5,3 +5,9 @@ from pyrogram import Client, filters
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
 	await message.reply_text("Bot :- @renameer2bot\nCreater :- @PiratesDeveloper\nLanguage :-Python3\nLibrary :- Pyrogram 1.4.16\nServer :- Heroku")
+	
+	
+		
+@Client.on_message(filters.private & filters.command(["clone"]))
+async def start(client,message):
+	await message.reply_text(">━━━━━━━━━᚜ PRIVATE BOT ᚛━━━━━━━━━<\nHi, Guys I'am a Developer, If You Need A Bot Like This => PM ME @PiratesDeveloper I Will Also Develope Bot Like Mallu_Movies/ProSearch bot and We Will Also Develope Bot As Your Wish😇\n>━━━━━━━━━᚜ PRIVATE BOT ᚛━━━━━━━━━<")
