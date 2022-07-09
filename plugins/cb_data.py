12773a01f61541386be78017347ac87998071439
Teampir <106901407+Teampir@users.noreply.github.com>
2022-07-09 03:03:19 +0100

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -30,10 +30,10 @@ async def doc(bot,update):
      new_filename = name[1]
      file_path = f"downloads/{new_filename}"
      file = update.message.reply_to_message
-     ms = await update.message.edit("``` Trying To Download...```")
+     ms = await update.message.edit("Trying To Download...")
      c_time = time.time()
      try:
-     	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "``` Trying To Download...```",  ms, c_time   ))
+     	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "Trying To Download...",  ms, c_time   ))
      except Exception as e:
      	await ms.edit(e)
      	return
@@ -51,10 +51,10 @@ async def doc(bot,update):
      		img.resize((320, 320))
      		img.save(ph_path, "JPEG")
      		c_time = time.time()
-     		await ms.edit("```Trying To Upload```")
+     		await ms.edit("Trying To Upload...")
      		c_time = time.time()
      		try:
-     			await bot.send_document(update.message.chat.id,document = file_path,thumb=ph_path,caption = f"**{new_filename}**",progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  ms, c_time   ))
+     			await bot.send_document(update.message.chat.id,document = file_path,thumb=ph_path,caption = f"{new_filename}",progress=progress_for_pyrogram,progress_args=( "Trying To Uploading...",  ms, c_time   ))
      			await ms.delete()
      			os.remove(file_path)
      			os.remove(ph_path)
@@ -64,10 +64,10 @@ async def doc(bot,update):
      			os.remove(ph_path)
      			     		     		
      else:
-     		await ms.edit("```Trying To Upload```")
+     		await ms.edit("Trying To Upload...")
      		c_time = time.time()
      		try:
-     			await bot.send_document(update.message.chat.id,document = file_path,caption = f"**{new_filename}**",progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  ms, c_time   ))
+     			await bot.send_document(update.message.chat.id,document = file_path,caption = f"{new_filename}",progress=progress_for_pyrogram,progress_args=( "Trying To Uploading...",  ms, c_time   ))
      			await ms.delete()
      			os.remove(file_path)
      		except Exception as e:
